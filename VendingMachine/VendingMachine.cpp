544392c46000b75c5fcf86994da8d262218700e2
Sabra Fisher <sabra.leigh.fisher@gmail.com>
2026-01-28 19:39:52 -0800

    added cases 4-7 for added vending machine items added default case for invalid selections.

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -9,27 +9,34 @@ int main()
         cin >> choice;
         switch (choice) {
         case 1:
-            cout << "Enjoy your Chips!" << endl;
+            cout << "Enjoy your chips!" << endl;
             break;
         case 2:
             cout << "Enjoy your Candy!" << endl;
             break;
         case 3:
-			cout << "Enjoy your Soda!" << endl;
+            cout << "Enjoy your Soda!" << endl;
+            break;
+        case 4:
+            cout << "Enjoy your Gum!" << endl;
+            break;
+        case 5:
+            cout << "Enjoy your Water!" << endl;
+            break;
+        case 6:
+            cout << "Enjoy your Mint!" << endl;
+            break;
+        case 7:
+            cout << "Enjoy your Cookie!" << endl;
+            break;
+        case 8:
+            cout << "Goodbye!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
             break;
         }
     } while (choice != 8);
     cout << "Thank you for Choosing VendByMo!" << endl;
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
