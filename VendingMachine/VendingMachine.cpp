ded32440010015b13d4c27dee9d548d0290024f6
Sabra Fisher <sabra.leigh.fisher@gmail.com>
2026-01-28 19:59:40 -0800

    added invalid input handler to fix endless loop error

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -7,36 +7,55 @@ int main()
     do {
         cout << "VendByMo Menu:\n  1. Chips\n  2. Candy\n  3. Soda\n  4. Gum\n  5. Water\n  6. Mint\n  7. Cookie\n  8. Exit\n\nEnter your choice: ";
         cin >> choice;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "\nINVALID INPUT: Please enter a numeric value(1-8).\n" << endl;
+            continue;
+        }
+
         switch (choice) {
         case 1:
-            cout << "Enjoy your chips!\n" << endl;
+            cout << "\nEnjoy your Chips!\n" << endl;
             break;
         case 2:
-            cout << "Enjoy your Candy!\n" << endl;
+            cout << "\nEnjoy your Candy!\n" << endl;
             break;
         case 3:
-            cout << "Enjoy your Soda!\n" << endl;
+            cout << "\nEnjoy your Soda!\n" << endl;
             break;
         case 4:
-            cout << "Enjoy your Gum!\n" << endl;
+            cout << "\nEnjoy your Gum!\n" << endl;
             break;
         case 5:
-            cout << "Enjoy your Water!\n" << endl;
+            cout << "\nEnjoy your Water!\n" << endl;
             break;
         case 6:
-            cout << "Enjoy your Mint!\n" << endl;
+            cout << "\nEnjoy your Mint!\n" << endl;
             break;
         case 7:
-            cout << "Enjoy your Cookie!\n" << endl;
+            cout << "\nEnjoy your Cookie!\n" << endl;
             break;
         case 8:
-            cout << "Goodbye!\n" << endl;
+            cout << "\nGoodbye!\n" << endl;
             break;
         default:
-            cout << "Invalid choice. Please try again." << endl;
-            break;
+            cout << "\nINVALID CHOICE: Please choose a number (1-8).\n" << endl;
+			break;
         }
     } while (choice != 8);
-    cout << "Thank you for Choosing VendByMo!" << endl;
+    cout << "\nThank you for Choosing VendByMo!" << endl;
     return 0;
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
